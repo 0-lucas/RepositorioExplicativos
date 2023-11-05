cc1f620d0d136f31d3122fc6828c78a48c8eafdd
0-lucas <luccleandro@hotmail.com>
2023-11-05 16:13:52 -0300

    Organização de diretórios e adição de códigos para transformadores no Scikit Learn.

---
+++ MachineLearning/ScikitLearn/custom_transformers.py
@@ -0,0 +1,36 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+
+
+# Usando Scikit Learn - Possível de integrar ao Pipeline. Boa prática e melhor deploy.
+class CombineAttributes(BaseEstimator, TransformerMixin):
+    def __init__(self, to_integer=True):
+        self.columns_ = None
+        self.column = "Dorm"
+        self.column_name = "Notebook per Dorm"
+        self.to_integer = to_integer
+
+    def fit(self, X=None, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X_new = X.groupby(self.column).sum()
+        X_new = X_new.rename(columns={X_new.columns[0]: self.column_name})  # Renomeando para melhor visualização.
+        X_merged = X.join(X_new, on=self.column)
+
+        if self.to_integer:
+            X_rounded = X_merged.astype({self.column_name: "Int8"})
+            return X_rounded
+
+        return X_merged
+
+
+# Criando uma funcão usando apenas pandas.
+def add_combined_column(dataframe, column, new_column_name="new_column"):
+    dataframe_grouped = dataframe.groupby(column).sum()
+
+    column_to_rename = dataframe_grouped.columns[0]
+    dataframe_renamed = dataframe_grouped.rename(columns={column_to_rename: new_column_name})
+
+    dataframe_merged = dataframe.join(dataframe_renamed, on="Dorm")
+    return dataframe_merged
