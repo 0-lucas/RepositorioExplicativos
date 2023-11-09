822fd602fbf413f8564b673f323005030604a123
Lucas <luccleandro@hotmail.com>
2023-11-09 18:06:37 -0300

    Refatoração de classes de transformação.

--- MachineLearning/ScikitLearn/custom_transformers.py
+++ MachineLearning/ScikitLearn/custom_transformers.py
@@ -13,18 +13,20 @@ class Square:
 
 # Usando Scikit Learn - Possível de integrar ao Pipeline. Boa prática e melhor deploy.
 class CombineAttributes(BaseEstimator, TransformerMixin):
-    def __init__(self, to_integer=True):
-        self.column = "Dorm"
-        self.column_name = "Notebook per Dorm"
+    def __init__(self, column="Dorm",
+                 column_name="Notebooks in Dorm", to_integer=True):
+
+        self.column = column
+        self.column_name = column_name
         self.to_integer = to_integer
 
     def fit(self, X=None, y=None):
         return self
 
     def transform(self, X, y=None):
-        X_new = X.groupby(self.column).sum()
-        X_new = X_new.rename(columns={X_new.columns[0]: self.column_name})  # Renomeando para melhor visualização.
-        X_merged = X.join(X_new, on=self.column)
+        X_new = X.groupby(self.column).sum(numeric_only=True)
+        X_rename = X_new.rename(columns={X_new.columns[0]: self.column_name})  # Renomeando para melhor visualização.
+        X_merged = X.join(X_rename, on=self.column)
 
         if self.to_integer:
             X_rounded = X_merged.astype({self.column_name: "Int8"})
@@ -35,7 +37,7 @@ class CombineAttributes(BaseEstimator, TransformerMixin):
 
 # Criando uma funcão usando apenas pandas.
 def add_combined_column(dataframe, column, new_column_name="new_column"):
-    dataframe_grouped = dataframe.groupby(column).sum()
+    dataframe_grouped = dataframe.groupby(column).sum(numeric_only=True)
 
     column_to_rename = dataframe_grouped.columns[0]
     dataframe_renamed = dataframe_grouped.rename(columns={column_to_rename: new_column_name})
